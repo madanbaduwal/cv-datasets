89dcf9b1492da48953b1f2471e7c38eb57dc3a8f
MadanBaduwal <67206974+MadanBaduwal@users.noreply.github.com>
2022-07-24 23:59:53 +0545

    .

--- scripts/search.js
+++ scripts/search.js
@@ -103,3 +103,23 @@ layout: null
 
 	displaySearchResults(window.index.search(query), query); // Hand the results off to be displayed
 })();
+
+
+function openGallery(id) {
+	closeAll();
+	const gallery = document.getElementById('gallery-'+id);
+	const card = document.getElementById('card-'+id);
+	gallery.classList.add('Gallery--active');
+	card.classList.add('Card--active');
+  }
+  
+  function closeAll() {
+	const galleryActv = document.querySelector('.Gallery--active');
+	const cardActv = document.querySelector('.Card--active');
+	if (galleryActv) {
+	  galleryActv.classList.remove('Gallery--active');
+	}
+	if (cardActv) {
+	  cardActv.classList.remove('Card--active');
+	}
+  }
